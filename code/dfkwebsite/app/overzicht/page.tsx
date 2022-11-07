8a26d0ede5f445396750c18d51ea70e9b4a64d27
Bryan-Deckers <r0889617@student.thomasmore.be>
2022-11-07 14:52:42 +0100

    paginas aangemaakt

---
+++ code/dfkwebsite/app/overzicht/page.tsx
@@ -0,0 +1,7 @@
+export default function Overzicht() {
+  return (
+    <h1 className="text-3xl font-bold underline text-blue-500">
+      Overzicht pagina
+    </h1>
+  );
+}
---
+++ code/dfkwebsite/app/wedstrijden/page.tsx
@@ -0,0 +1,7 @@
+export default function Wedstrijden() {
+  return (
+    <h1 className="text-3xl font-bold underline text-blue-500">
+      Wedstrijden pagina
+    </h1>
+  );
+}
