f919708bd4536f8f9aac084d84106a34a452adfb
Bryan <r0889617@student.thomasmore.be>
2022-11-10 08:57:38 +0100

    fixed conflicts

--- code/dfkwebsite/app/page.tsx
+++ code/dfkwebsite/app/page.tsx
@@ -3,17 +3,11 @@ import ImageRead from './ImageRead';
 
 export default function Page() {
   return (
-<<<<<<< Updated upstream
-    <h1 className="text-3xl font-bold underline text-blue-500">
-      Hello, Next.js!
-    </h1>
-=======
     <div>
       <h1 className="text-3xl font-bold underline text-red-500">
         Hello, Next.js!
         <ImageRead />
       </h1>
     </div>
->>>>>>> Stashed changes
   );
 }
