0b1460a7f31fb682c216cd33f72adf08754b2830
Bryan <r0889617@student.thomasmore.be>
2022-11-10 09:01:52 +0100

    added nav in homepage

---
+++ code/dfkwebsite/app/NavItem.tsx
@@ -0,0 +1,20 @@
+'use client';
+
+import Link from "next/link";
+import { Icon } from "@iconify/react";
+
+type MyProps = {
+  href: string;
+  dropdown?: boolean;
+};
+
+export default function NavItem(props: MyProps) {
+  return (
+    <li className="hover:underline hover:underline-offset-[7px] hover:decoration-[3px] hover:decoration-white text-[#fff]">
+      <Link href={"/" + props.href} className="capitalize flex items-center">
+        {props.href}
+        {props.dropdown ? <Icon icon="ic:baseline-arrow-drop-down" className="text-3xl" /> : null}
+      </Link>
+    </li>
+  );
+}
