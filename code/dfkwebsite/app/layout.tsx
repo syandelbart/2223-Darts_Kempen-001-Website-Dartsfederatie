d331d6a4d891f69dcc68b7fb1f76bc2be9b45570
Bryan-Deckers <r0889617@student.thomasmore.be>
2022-11-10 01:00:43 +0100

    changes

--- code/dfkwebsite/app/layout.tsx
+++ code/dfkwebsite/app/layout.tsx
@@ -1,15 +1,32 @@
 // These styles apply to every route in the application
 import './globals.css';
+<<<<<<< Updated upstream
+=======
+import Navbar from './Navbar';
+import Footer from './Footer';
+
+import { Montserrat } from '@next/font/google';
+
+const montserrat = Montserrat();
+>>>>>>> Stashed changes
 
 export default function RootLayout({ children }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={montserrat.className}>
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>
+<<<<<<< Updated upstream
       <body>{children}</body>
+=======
+      <body className="container mx-auto bg-background">
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
+>>>>>>> Stashed changes
     </html>
   );
 }
---
+++ code/dfkwebsite/app/overzicht/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Page() {
+  return (
+    <h1 className="text-3xl font-bold underline text-blue-500 h-56">
+      Overzicht pagina
+    </h1>
+  );
+}
