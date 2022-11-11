eecff9556cdef00c9c0f0c57b11071bbfa1955ee
Bryan-Deckers <r0889617@student.thomasmore.be>
2022-11-11 20:26:52 +0100

    Footer links in component

--- code/dfkwebsite/app/Footer.tsx
+++ code/dfkwebsite/app/Footer.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import dfkLogo from "../public/dfklogo.png";
 import Link from "next/link";
+import FooterLink from "./FooterLink";
 
 // Functionaliteit toevoegen van dropdown optie
 export default function Footer() {
@@ -10,26 +11,34 @@ export default function Footer() {
         <div>
           <h1 className="text-2xl mb-1">Navigatie</h1>
           <ul>
-            <li>  <Link href="#">Linknaam</Link>  </li>
-            <li>  <Link href="#">Linknaam</Link>  </li>
-            <li>  <Link href="#">Linknaam</Link>  </li>
+            <FooterLink href="overzicht"  />
+            <FooterLink href="info"  />
+            <FooterLink href="wedstrijden"  />
+            <FooterLink href="uitslagen"  />
+            <FooterLink href="contact"  />
           </ul>
         </div>
 
-        <Image className="flex-grow-0" src={dfkLogo} alt="dfk logo" width={250} height={250}  />
+        <Image
+          className="flex-grow-0"
+          src={dfkLogo}
+          alt="dfk logo"
+          width={250}
+          height={250}
+        />
 
         <div>
           <h1 className="text-2xl text-right mb-1">Nuttige links</h1>
           <ul className="text-right">
-            <li>  <Link href="#">Privacyverklaring</Link>  </li>
-            <li>  <Link href="#">Privacyverklaring</Link>  </li>
-            <li>  <Link href="#">Privacyverklaring</Link>  </li>
+            <FooterLink href="privacyverklaring"  />
+            <FooterLink href="privacyverklaring"  />
+            <FooterLink href="privacyverklaring"  />
           </ul>
         </div>
       </div>
 
       <div className="w-100 flex justify-between text-1xl mt-16 font-medium">
-        <p>Darts Federatie Kempen &copy; {(new Date()).getFullYear()}</p>
+        <p>Darts Federatie Kempen &copy; {new Date().getFullYear()}</p>
         <p>Gemaakt door Barry en Dekkers</p>
       </div>
     </footer>
