43a31917b3a6191713272dce77ac5157a40e3f3c
Syan Delbart <syan.delbart@hotmail.com>
2022-11-14 15:38:13 +0100

    design stuff

--- code/dfkwebsite/app/ImageRead.tsx
+++ code/dfkwebsite/app/ImageRead.tsx
@@ -9,12 +9,12 @@ type MyProps = {
 
 export default function ImageRead(props: MyProps) {
   return (
-    <div className="h-[500px] flex flex-row w-full my-10 gap-20 justify-between">
-      <div className={`min-w-[700px] max-w-[700px] h-full flex-grow relative overflow-hidden ${props.order ? 'order-1': 'order-0'}`}>
+    <div className="h-[500px] flex flex-row w-full my-32 gap-20">
+      <div className={`w-[700px] min-w-[700px] max-w-[700px] h-full flex-grow relative overflow-hidden ${props.order ? 'order-1': 'order-0'}`}>
         <Image src={dfkLogo} alt="test" className="bg-[#676767] object-cover" fill/>
       </div>
       <div className={`flex flex-col flex-grow text-white ${props.order ? 'order-0': 'order-1 text-left' }`}>
-        <h1 className="font-extrabold text-5xl">{props.title}</h1>
+        <h1 className="54442 font-extrabold text-5xl">{props.title}</h1>
         <p className="text-2xl" >{props.summary}</p>
       </div>
     </div>
