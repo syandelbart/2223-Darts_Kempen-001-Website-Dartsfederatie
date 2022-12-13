05363ecfefcfc2644ae7fb394ee55e45c18c9140
Bryan <r0889617@student.thomasmore.be>
2022-12-13 14:37:38 +0100

    layout tijdelijk gefixt

--- code/dfkwebsite/components/Layout.tsx
+++ code/dfkwebsite/components/Layout.tsx
@@ -1,5 +1,4 @@
 // These styles apply to every route in the application
-import './globals.css';
 import Navbar from './Navbar';
 import Footer from './Footer';
 
@@ -7,22 +6,14 @@ import { Montserrat } from '@next/font/google';
 
 const montserrat = Montserrat();
 
-export default function RootLayout({ children }: {
-  children: React.ReactNode;
-}) {
+export default function Layout({ children }: any) {
   return (
-    <html lang="nl" className={montserrat.className}>
-      <head>
-        <title>DFK - Home</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      </head>
-      <body className='bg-background'>
-        <Navbar />
-        <main className="container mx-auto">
-          {children}
-        </main>
-        <Footer />
-      </body>
-    </html>
-  );
+    <>
+      <Navbar />
+      <main className="bg-background container mx-auto">
+        {children}
+      </main>
+      <Footer />
+    </>
+  )
 }
---
+++ code/dfkwebsite/pages/_app.tsx
@@ -0,0 +1,10 @@
+import '../styles/globals.css';
+import Layout from "../components/Layout";
+
+export default function MyApp({ Component, pageProps }: any) {
+  return (
+    <Layout >
+      <Component {...pageProps} />
+    </Layout>
+  );
+}
