e83341f1b929dcfa8f582f74b43d8c30d0d05e8c
Bryan <r0889617@student.thomasmore.be>
2022-12-16 11:28:35 +0100

    profiel menu is er ma nie schoon

--- code/dfkwebsite/components/ProfileMenu.tsx
+++ code/dfkwebsite/components/ProfileMenu.tsx
@@ -1,34 +1,35 @@
 import { Icon } from "@iconify/react";
+import Link from "next/link";
 import { useState } from "react";
 
 export default function ProfileMenu() {
   const [isOpen, setIsOpen] = useState(false);
   return (
-    <div
-      className="relative"
+    <li
+      className=" text-[#fff] xl:text-xl relative hover:children:visible"
       onMouseEnter={() => setIsOpen(true)}
-      onMouseOut={(e) => {}}
+      onMouseLeave={() => setIsOpen(false)}
     >
       <Icon
         icon="healthicons:ui-user-profile-outline"
         className="text-5xl bg-white rounded-[50%] text-black cursor-pointer "
       />
-      {isOpen && (
-        <div className="absolute top-20 right-0 bg-white rounded-lg shadow-lg w-56 h-40 flex flex-col gap-2 justify-center items-center">
-          <div className="flex items-center justify-evenly cursor-pointer w-full">
-            <Icon icon="mdi:account" className="text-3xl" />
-            <p>Profiel</p>
-          </div>
-          <div className="flex items-center justify-evenly cursor-pointer w-full">
-            <Icon icon="mdi:logout" className="text-3xl" />
-            <p>Uitloggen</p>
-          </div>
-          <div className="flex items-center justify-evenly cursor-pointer w-full">
-            <Icon icon="mdi:weather-night" className="text-3xl" />
-            <p>Dark mode</p>
-          </div>
+      <div
+        className={`absolute top-[100%] right-0 flex flex-col p-4 bg-gray-500 ${isOpen ? "visible" : "invisible"}`}
+      >
+        <div className="flex cursor-pointer">
+          <Icon icon="mdi:account" className="text-3xl" />
+          <p>Profiel</p>
         </div>
-      )}
-    </div>
+        <div className="flex cursor-pointer">
+          <Icon icon="mdi:logout" className="text-3xl" />
+          <p>Uitloggen</p>
+        </div>
+        <div className="flex cursor-pointer">
+          <Icon icon="mdi:weather-night" className="text-3xl" />
+          <p>Dark mode</p>
+        </div>
+      </div>
+    </li>
   );
 }
