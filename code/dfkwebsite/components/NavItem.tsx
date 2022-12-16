bc6d578bf71b2bffd85df50f42313803d224708b
Bryan <r0889617@student.thomasmore.be>
2022-12-16 10:45:39 +0100

    format file

--- code/dfkwebsite/components/NavItem.tsx
+++ code/dfkwebsite/components/NavItem.tsx
@@ -27,24 +27,23 @@ export default function NavItem(props: MyProps) {
           <Icon icon="ic:baseline-arrow-drop-down" className="text-4xl" />
         ) : null}
       </Link>
-      {props.dropdown? (
-                <div
-                  className={`absolute top-[100%] flex flex-col ${isOpen ? "visible" : "invisible"}`}
-                >
-                  {props.dropdown.map((item: any) => {
-                    return (
-                      <Link
-                        href={"/" + item.href}
-                        className="capitalize hover:text-accent p-4 bg-gray-500 border-b border-blue-100"
-                        key={item.title}
-                      >
-                        {item.title}
-                      </Link>
-                    );
-                  })}
-                </div>
-              ) : null}
-
+      {props.dropdown ? (
+        <div
+          className={`absolute top-[100%] flex flex-col ${isOpen ? "visible" : "invisible"}`}
+        >
+          {props.dropdown.map((item: any) => {
+            return (
+              <Link
+                href={"/" + item.href}
+                className="capitalize hover:text-accent p-4 bg-gray-500 border-b border-blue-100"
+                key={item.title}
+              >
+                {item.title}
+              </Link>
+            );
+          })}
+        </div>
+      ) : null}
     </li>
   );
 }
