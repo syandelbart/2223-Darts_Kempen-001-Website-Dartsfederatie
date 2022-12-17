73710ca90b7b39b1371a81ea6cef73273eed397f
Bryan-Deckers <r0889617@student.thomasmore.be>
2022-12-17 17:06:58 +0100

    format overzicht

--- code/dfkwebsite/pages/overzicht/index.tsx
+++ code/dfkwebsite/pages/overzicht/index.tsx
@@ -3,14 +3,28 @@ import Selectie from "../../components/Selectie";
 export default function Overzicht() {
   return (
     <div>
-      <h1 className="text-5xl mb-20 text-white font-bold">
-        Overzicht
-      </h1>
+      <h1 className="text-5xl mb-20 text-white font-bold">Overzicht</h1>
       <div className="flex gap-20">
-        <Selectie href="/overzicht/clubs" title="clubs" icon="game-icons:dart" />
-        <Selectie href="/overzicht/teams" title="teams" icon="mdi:account-group-outline" />
-        <Selectie href="/overzicht/spelers" title="spelers" icon="mdi:person-outline" />
-        <Selectie href="/overzicht/bestuur" title="bestuur" icon="icon-park-outline:user-business" />
+        <Selectie
+          href="/overzicht/clubs"
+          title="clubs"
+          icon="game-icons:dart"
+        />
+        <Selectie
+          href="/overzicht/teams"
+          title="teams"
+          icon="mdi:account-group-outline"
+        />
+        <Selectie
+          href="/overzicht/spelers"
+          title="spelers"
+          icon="mdi:person-outline"
+        />
+        <Selectie
+          href="/overzicht/bestuur"
+          title="bestuur"
+          icon="icon-park-outline:user-business"
+        />
       </div>
     </div>
   );
