5bde182eb8cfad7f8ddf265fea6728618473b665
Bryan-Deckers <r0889617@student.thomasmore.be>
2022-12-17 17:07:07 +0100

    merge conflicts fixed

--- code/dfkwebsite/pages/competitie/index.tsx
+++ code/dfkwebsite/pages/competitie/index.tsx
@@ -1,5 +1,27 @@
+import Selectie from "../../components/Selectie";
+
 export default function Competitie() {
   return (
-    <h1 className="text-6xl font-extrabold text-white mb-5">Competitie pagina</h1>
+    <div>
+      <h1 className="text-6xl mb-20 text-white font-bold">Competitie</h1>
+      <div className="flex gap-20">
+        <Selectie
+          href="/competitie/speeldagen"
+          title="speeldagen"
+          icon="fluent:news-16-regular"
+        />
+        <Selectie href="/info/kalender" title="kalender" icon="uil:calender" />
+        <Selectie
+          href="/competitie/klassement"
+          title="klassement"
+          icon="mingcute:documents-line"
+        />
+        <Selectie
+          href="/competitie/individueleranking"
+          title="individueleranking"
+          icon="mdi:file-document-edit-outline"
+        />
+      </div>
+    </div>
   );
 }
--- code/dfkwebsite/pages/info/index.tsx
+++ code/dfkwebsite/pages/info/index.tsx
@@ -1,16 +1,26 @@
 import Selectie from "../../components/Selectie";
 
-export default function Inschrijvingen() {
+export default function Info() {
   return (
     <div>
-      <h1 className="text-6xl mb-20 text-white font-bold">
-        Info
-      </h1>
+      <h1 className="text-6xl mb-20 text-white font-bold">Info</h1>
       <div className="flex gap-20">
-        <Selectie href="/info/nieuws" title="nieuws" icon="fluent:news-16-regular" />
+        <Selectie
+          href="/info/nieuws"
+          title="nieuws"
+          icon="fluent:news-16-regular"
+        />
         <Selectie href="/info/kalender" title="kalender" icon="uil:calender" />
-        <Selectie href="/info/documenten" title="documenten" icon="mingcute:documents-line" />
-        <Selectie href="/info/inschrijvingen" title="inschrijvingen" icon="mdi:file-document-edit-outline" />
+        <Selectie
+          href="/info/documenten"
+          title="documenten"
+          icon="mingcute:documents-line"
+        />
+        <Selectie
+          href="/info/inschrijvingen"
+          title="inschrijvingen"
+          icon="mdi:file-document-edit-outline"
+        />
       </div>
     </div>
   );
