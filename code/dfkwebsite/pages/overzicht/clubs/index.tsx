b5a43d998fe70af5ebf8798e5ae9b84c93c695d7
Bryan-Deckers <r0889617@student.thomasmore.be>
2022-12-18 12:39:28 +0100

    array met data en map

--- code/dfkwebsite/pages/overzicht/clubs/index.tsx
+++ code/dfkwebsite/pages/overzicht/clubs/index.tsx
@@ -1,9 +1,73 @@
+import Club from "../../../components/overzicht/Club";
+
+let clubs = [
+  {
+    clubnaam: "Club 1",
+    clubplek: "Clubplek",
+    straatnaam: "Straatnaam",
+    postcode: "1234",
+  },
+  {
+    clubnaam: "Club 2",
+    clubplek: "Clubplek",
+    straatnaam: "Straatnaam",
+    postcode: "1234",
+  },
+  {
+    clubnaam: "Club 3",
+    clubplek: "Clubplek",
+    straatnaam: "Straatnaam",
+    postcode: "1234",
+  },
+  {
+    clubnaam: "Club 4",
+    clubplek: "Clubplek",
+    straatnaam: "Straatnaam",
+    postcode: "1234",
+  },
+  {
+    clubnaam: "Club 5",
+    clubplek: "Clubplek",
+    straatnaam: "Straatnaam",
+    postcode: "1234",
+  },
+  {
+    clubnaam: "Club 6",
+    clubplek: "Clubplek",
+    straatnaam: "Straatnaam",
+    postcode: "1234",
+  },
+  {
+    clubnaam: "Club 7",
+    clubplek: "Clubplek",
+    straatnaam: "Straatnaam",
+    postcode: "1234",
+  },
+]
+
 export default function Clubs() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline text-blue-500 h-56">
-        Clubs pagina
-      </h1>
+      <div className="flex justify-between items-center mb-10">
+        <h1 className="text-6xl font-extrabold text-white">Clubs</h1>
+        <input
+          type="text"
+          placeholder="Zoeken..."
+          className="px-5 py-3 rounded bg-[#D9D9D9]"
+        />
+      </div>
+      <div className="grid grid-cols-3 gap-5">
+        {
+        clubs.map((club) => (
+          <Club
+            clubnaam={club.clubnaam}
+            clubplek={club.clubplek}
+            straatnaam={club.straatnaam}
+            postcode={club.postcode}
+          />
+        ))
+        }
+      </div>
     </div>
   );
 }
