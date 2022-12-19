c61af03272aae6a6201687f1d3ef86ddf08e276a
Bryan-Deckers <r0889617@student.thomasmore.be>
2022-12-19 13:03:56 +0100

    search functionality wip

--- code/dfkwebsite/pages/overzicht/clubs/index.tsx
+++ code/dfkwebsite/pages/overzicht/clubs/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Club from "../../../components/overzicht/club/Club";
 import Search from "../../../components/overzicht/Search";
 
@@ -44,26 +45,53 @@ let clubs = [
     straatnaam: "Straatnaam",
     postcode: "1234",
   },
-]
+];
 
 export default function Clubs() {
+  const [search, setSearch] = useState("");
+  let results = 0;
   return (
     <div>
       <div className="flex justify-between items-center mb-10">
         <h1 className="text-6xl font-extrabold text-white">Clubs</h1>
-        <Search />
+        <input
+          type="text"
+          placeholder="Zoeken..."
+          className="px-5 py-3 rounded bg-[#D9D9D9]"
+          onChange={(e) => setSearch(e.target.value)}
+        />
       </div>
       <div className="grid grid-cols-3 gap-5">
-        {
-        clubs.map((club) => (
-          <Club
-            clubnaam={club.clubnaam}
-            clubplek={club.clubplek}
-            straatnaam={club.straatnaam}
-            postcode={club.postcode}
-          />
-        ))
-        }
+        {clubs.length === 0 ? (
+          <h1 className="text-4xl font-extrabold text-white">
+            Geen clubs gevonden
+          </h1>
+        ) : (
+          clubs
+            .filter((club) => {
+              if (search == "") {
+                return club;
+              } else if (
+                club.clubnaam.toLowerCase().includes(search.toLowerCase())
+              ) {
+                return club;
+              }
+              results++;
+            })
+            .map((club) => (
+              <Club
+                clubnaam={club.clubnaam}
+                clubplek={club.clubplek}
+                straatnaam={club.straatnaam}
+                postcode={club.postcode}
+              />
+            ))
+        )}
+        {results === clubs.length && (
+          <h1 className="text-4xl font-extrabold text-white">
+            Geen clubs gevonden
+          </h1>
+        )}
       </div>
     </div>
   );
