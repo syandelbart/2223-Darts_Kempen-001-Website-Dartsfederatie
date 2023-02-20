f28236ec101ec63f2980add0cf7d774843120e38
Bryan-Deckers <r0889617@student.thomasmore.be>
2023-02-20 22:13:45 +0100

    login modal nicer

--- code/dfkwebsite/components/LoginModal.tsx
+++ code/dfkwebsite/components/LoginModal.tsx
@@ -11,13 +11,13 @@ const LoginModal : FunctionComponent<LoginModalData> = (props: LoginModalData) =
     <div className={`${props.isOpen ? "" : "hidden"}`}>
       <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 z-50"></div>
       <div className="fixed top-0 left-0 w-full h-full z-50 flex justify-center items-center">
-        <div className="bg-background w-1/4 rounded-2xl px-10 pb-10 pt-5">
+        <div className="bg-background w-1/4 rounded-2xl p-10">
           <div className="flex justify-between items-center text-white">
-            <h1 className="text-3xl font-semibold">Inloggen</h1>
+            <h1 className="text-4xl font-semibold">Inloggen</h1>
             <Icon icon="mdi:close" className="text-3xl hover:text-red-500" onClick={() => props.setIsOpen(!props.isOpen)} />
           </div>
           <div className="flex flex-col">
-            <label htmlFor="email" className="text-white mt-16 mb-2">
+            <label htmlFor="email" className="text-xl text-white mt-16 mb-2">
               Email
             </label>
             <input
@@ -27,7 +27,7 @@ const LoginModal : FunctionComponent<LoginModalData> = (props: LoginModalData) =
               placeholder="email"
               className="bg-gray-200 p-2"
             />
-            <label htmlFor="password" className="text-white mt-5 mb-2">
+            <label htmlFor="password" className="text-xl text-white mt-5 mb-2">
               Wachtwoord
             </label>
             <input
@@ -37,10 +37,10 @@ const LoginModal : FunctionComponent<LoginModalData> = (props: LoginModalData) =
               placeholder="wachtwoord"
               className="bg-gray-200 p-2"
             />
-            <button className="bg-green-700 text-white rounded-md p-2 mt-10">
+            <button className="bg-[#0A893D] text-white rounded-lg p-3 mt-10">
                 Inloggen
             </button>
-            <button className="bg-gray-500 text-white rounded-md p-2 mt-3">
+            <button className="bg-[#686F6B] text-white rounded-lg p-3 mt-3">
                 Registreren
             </button>
           </div>
