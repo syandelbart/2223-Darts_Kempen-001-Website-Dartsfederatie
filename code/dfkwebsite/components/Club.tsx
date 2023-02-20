984a6cfd692abab818ee027c92be75f1e6c8b76c
Bryan-Deckers <r0889617@student.thomasmore.be>
2023-02-20 23:05:07 +0100

    club modal design

--- code/dfkwebsite/components/Club.tsx
+++ code/dfkwebsite/components/Club.tsx
@@ -14,7 +14,7 @@ const Club : FunctionComponent<clubData> = (props: clubData) => {
       <p className="mb-4 text-3xl font-bold">{props.clubnaam}</p>
       <div className="flex gap-8 children:hover:cursor-pointer">
         <p className="rounded-2xl px-8 py-1 bg-[#676767]" onClick={() => props.setIsOpen(true)}>Teams</p>
-        <p className="rounded-2xl px-10 py-1 bg-[#95A4F3]">Edit</p>
+        <p className="rounded-2xl px-10 py-1 bg-edit-button">Edit</p>
       </div>
       <div className="my-3">
         <p>{props.clubplek}</p>
