d6c654c78aeb239284fa80c552e213f2f7f15f38
Syan Delbart <syan.delbart@hotmail.com>
2023-02-21 14:39:49 +0100

    Oopsie :)

--- code/dfkwebsite/components/CardButton.tsx
+++ code/dfkwebsite/components/CardButton.tsx
@@ -9,7 +9,7 @@ type buttonData = {
 
 const CardButton : FunctionComponent<buttonData> = ({children,font = "text-sm",bg = "bg-[#676767]",onClick}) => {
   return (
-    <button className={`cursor-pointer rounded-2xl px-10 py-1 ${fontSize} ${bg}`} onClick={onClick}>
+    <button className={`cursor-pointer rounded-2xl px-10 py-1 ${font} ${bg}`} onClick={onClick}>
         {children}
     </button>
   );
