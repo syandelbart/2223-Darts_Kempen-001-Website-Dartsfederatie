d8434612ff270429fc2bf428fc7399716ab2c142
Syan Delbart <syan.delbart@hotmail.com>
2023-02-21 13:30:12 +0100

    Tournament added

--- code/dfkwebsite/pages/index.tsx
+++ code/dfkwebsite/pages/index.tsx
@@ -6,6 +6,10 @@ const HomePage: NextPage = () => {
     <div>
       <ImageRead title="This is a title" summary="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. " date={1}/>
       <ImageRead title="This is a title" summary="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,  quis nostrud exercitation ullamco laboris..." order={true} date={1}/>
+      <div>
+        <h1 className=" font-extrabold text-5xl mb-1 text-white">Toernooi in de kijker</h1>
+        <div className="min-h-[200vh] w-full bg-[#676767]"></div>
+      </div>
     </div>
   );
 }
