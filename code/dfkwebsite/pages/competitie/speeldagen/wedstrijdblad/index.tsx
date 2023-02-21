18adcb0a462fe28975f637923bfd069e26763555
Bryan <r0889617@student.thomasmore.be>
2023-02-21 12:24:09 +0100

    page created

---
+++ code/dfkwebsite/pages/competitie/speeldagen/wedstrijdblad/index.tsx
@@ -0,0 +1,9 @@
+import { NextPage } from "next";
+
+const Wedstrijdblad : NextPage = () => {
+    return (
+        <h1 className="text-6xl font-extrabold text-white mb-5">Wedstrijdblad pagina</h1>
+    );
+}  
+
+export default Wedstrijdblad;
