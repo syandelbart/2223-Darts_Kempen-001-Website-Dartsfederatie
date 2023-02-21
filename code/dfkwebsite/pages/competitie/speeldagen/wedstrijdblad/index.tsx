9392d83d652a3890d1bf80dd318c2341d0fa6c8f
Bryan <r0889617@student.thomasmore.be>
2023-02-21 16:37:06 +0100

    Update index.tsx

--- code/dfkwebsite/pages/competitie/speeldagen/wedstrijdblad/index.tsx
+++ code/dfkwebsite/pages/competitie/speeldagen/wedstrijdblad/index.tsx
@@ -9,37 +9,23 @@ const Wedstrijdblad: NextPage = () => {
                 <p className="font-semibold">BEZOEKERS: DFK 2</p>
                 <p className="bg-background">NAAM EN VOORNAAM</p>
                 <p className="bg-background">NAAM EN VOORNAAM</p>
-                <p>1. Naam Voornaam</p>
-                <p>1. Naam Voornaam</p>
-                <p>2. Naam Voornaam</p>
-                <p>2. Naam Voornaam</p>
-                <p>3. Naam Voornaam</p>
-                <p>3. Naam Voornaam</p>
-                <p>4. Naam Voornaam</p>
-                <p>4. Naam Voornaam</p>
-                <p>5. Naam Voornaam</p>
-                <p>5. Naam Voornaam</p>
-                <p>6. Naam Voornaam</p>
-                <p>6. Naam Voornaam</p>
-                <p>7. Naam Voornaam</p>
-                <p>7. Naam Voornaam</p>
-                <p>8. Naam Voornaam</p>
-                <p>8. Naam Voornaam</p>
-                <p>9. Naam Voornaam</p>
-                <p>9. Naam Voornaam</p>
-                <p>10. Naam Voornaam</p>
-                <p>10. Naam Voornaam</p>
-                <p>11. Naam Voornaam</p>
-                <p>11. Naam Voornaam</p>
-                <p>12. Naam Voornaam</p>
-                <p>12. Naam Voornaam</p>
+                {
+                    [...Array(12)].map((e, i) => {
+                        return (
+                            <>
+                                <p>{i+1}. Naam Voornaam</p>
+                                <p>{i+1}. Naam Voornaam</p>
+                            </>
+                        )
+                    })
+                }
             </div>
 
-            <div className="grid grid-cols-12 bg-nav-background children:border children:pl-5 children:py-2 children:text-center">
+            <div className="grid grid-cols-12 bg-nav-background children:border children:py-2 children:text-center">
                 <p className="col-span-5 bg-background">NAAM EN VOORNAAM</p>
                 <p className="col-span-2 bg-background">UITSLAG</p>
                 <p className="col-span-5 bg-background">NAAM EN VOORNAAM</p>
-                
+
                 <p>180</p>
                 <p className="col-span-2">ENKELSPELEN</p>
                 <p>KLEG</p>
@@ -49,6 +35,135 @@ const Wedstrijdblad: NextPage = () => {
                 <p>KLEG</p>
                 <p className="col-span-2">ENKELSPELEN</p>
                 <p>180</p>
+
+                <p>1</p>
+                <p className="col-span-2">Naam Voornaam</p>
+                <p>0</p>
+                <p>0</p>
+                <p>0</p>
+                <p>0</p>
+                <p>0</p>
+                <p>0</p>
+                <p className="col-span-2">Naam Voornaam</p>
+                <p>1</p>
+
+                <p>1</p>
+                <p className="col-span-2">Naam Voornaam</p>
+                <p>0</p>
+                <p>0</p>
+                <p>0</p>
+                <p>0</p>
+                <p>0</p>
+                <p>0</p>
+                <p className="col-span-2">Naam Voornaam</p>
+                <p>1</p>
+
+                <p>180</p>
+                <p className="col-span-2">DUBBELS</p>
+                <p>KLEG</p>
+                <p>H.U.</p>
+                <p className="col-span-2">Best of 5 - 701</p>
+                <p>H.U.</p>
+                <p>KLEG</p>
+                <p className="col-span-2">DUBBELS</p>
+                <p>180</p>
+
+                <p>1</p>
+                <p className="col-span-2">Naam Voornaam</p>
+                <p>0</p>
+                <p>0</p>
+                <p className="row-span-2">0</p>
+                <p className="row-span-2">0</p>
+                <p>0</p>
+                <p>0</p>
+                <p className="col-span-2">Naam Voornaam</p>
+                <p>1</p>
+
+                <p>1</p>
+                <p className="col-span-2">Naam Voornaam</p>
+                <p>0</p>
+                <p>0</p>
+                <p>0</p>
+                <p>0</p>
+                <p className="col-span-2">Naam Voornaam</p>
+                <p>1</p>
+
+                <p>1</p>
+                <p className="col-span-2">Naam Voornaam</p>
+                <p>0</p>
+                <p>0</p>
+                <p className="row-span-2">0</p>
+                <p className="row-span-2">0</p>
+                <p>0</p>
+                <p>0</p>
+                <p className="col-span-2">Naam Voornaam</p>
+                <p>1</p>
+
+                <p>1</p>
+                <p className="col-span-2">Naam Voornaam</p>
+                <p>0</p>
+                <p>0</p>
+                <p>0</p>
+                <p>0</p>
+                <p className="col-span-2">Naam Voornaam</p>
+                <p>1</p>
+
+                <p>180</p>
+                <p className="col-span-2">ENKELSPELEN</p>
+                <p>KLEG</p>
+                <p>H.U.</p>
+                <p className="col-span-2">Best of 7 - 501</p>
+                <p>H.U.</p>
+                <p>KLEG</p>
+                <p className="col-span-2">ENKELSPELEN</p>
+                <p>180</p>
+
+                <p>1</p>
+                <p className="col-span-2">Naam Voornaam</p>
+                <p>0</p>
+                <p>0</p>
+                <p>0</p>
+                <p>0</p>
+                <p>0</p>
+                <p>0</p>
+                <p className="col-span-2">Naam Voornaam</p>
+                <p>1</p>
+
+                <p>1</p>
+                <p className="col-span-2">Naam Voornaam</p>
+                <p>0</p>
+                <p>0</p>
+                <p>0</p>
+                <p>0</p>
+                <p>0</p>
+                <p>0</p>
+                <p className="col-span-2">Naam Voornaam</p>
+                <p>1</p>
+
+                <p className="col-span-5 border-b-0">Notities</p>
+                <p className="col-span-2">EINDUITSLAG</p>
+                <p className="col-span-5 border-b-0">Notities</p>
+
+                <p className="col-span-5 border-y-0"></p>
+                <p className="col-span-1">4</p>
+                <p className="col-span-1">6</p>
+                <p className="col-span-5 border-y-0"></p>
+
+                <p className="col-span-5 border-y-0"></p>
+                <p className="col-span-1">4</p>
+                <p className="col-span-1">6</p>
+                <p className="col-span-5 border-y-0"></p>
+
+                <p className="col-span-5 border-t-0"></p>
+                <p className="col-span-1">4</p>
+                <p className="col-span-1">6</p>
+                <p className="col-span-5 border-t-0"></p>
+
+                <div className="col-span-12">
+                    <p>Definitief Opslaan</p>
+                    <input type="checkbox" name="" id="" />
+                </div>
+                <a href="" className="col-span-12">Opslaan</a>
             </div>
         </div>
     );
