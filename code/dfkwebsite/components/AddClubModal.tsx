36a95d52c03707c1bd3a6f231b16ea69e6345c08
Bryan Deckers <r0889617@student.thomasmore.be>
2023-05-05 21:43:17 +0200

    front end club add modal

--- code/dfkwebsite/components/AddClubModal.tsx
+++ code/dfkwebsite/components/AddClubModal.tsx
@@ -2,71 +2,126 @@ import { Icon } from "@iconify/react";
 import { FunctionComponent } from "react";
 
 type AddClubModalData = {
-    addModalOpen: boolean;
-    setAddModalOpen: any;
-}
+  addModalOpen: boolean;
+  setAddModalOpen: any;
+};
 
-const AddClubModal: FunctionComponent<AddClubModalData> = (props: AddClubModalData) => {
-    return (
-        <div className={`${props.addModalOpen ? "" : "hidden"}`}>
-            <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 z-50"></div>
-            <div className="fixed top-0 left-0 w-full h-full z-50 flex justify-center items-center">
-                <div className="bg-background w-1/4 rounded-2xl p-10">
-                    <div className="flex justify-between items-center text-white">
-                        <h1 className="text-4xl font-semibold">Club toevoegen</h1>
-                        <Icon icon="mdi:close" className="text-3xl hover:text-red-500 hover:cursor-pointer" onClick={() => props.setAddModalOpen(!props.addModalOpen)} />
-                    </div>
-                    <div className="flex flex-col">
-                        <label htmlFor="firstname" className="text-xl text-white mt-16 mb-2">
-                            Voornaam
-                        </label>
-                        <input
-                            type="text"
-                            name="firstname"
-                            id="firstname"
-                            placeholder="Voornaam"
-                            className="bg-gray-200 p-2"
-                        />
-                        <label htmlFor="lastname" className="text-xl text-white mt-5 mb-2">
-                            Achternaam
-                        </label>
-                        <input
-                            type="text"
-                            name="lastname"
-                            id="lastname"
-                            placeholder="Achternaam"
-                            className="bg-gray-200 p-2"
-                        />
-                        <label htmlFor="phone" className="text-xl text-white mt-5 mb-2">
-                            Telefoonnummer
-                        </label>
-                        <input
-                            type="text"
-                            name="phone"
-                            id="phone"
-                            placeholder="Telefoonnummer"
-                            className="bg-gray-200 p-2"
-                        />
-                        <div className="mt-5 mb-2">
-                            <label htmlFor="allowedToPlay" className="text-xl text-white mr-3">
-                                Speelgerechtigd
-                            </label>
-                            <input
-                                type="checkbox"
-                                name="allowedToPlay"
-                                id="allowedToPlay"
-                                placeholder="Speelgerechtigd"
-                                className="bg-gray-200 p-2"
-                            />
-                        </div>
-                        <button className="bg-[#0A893D] text-white rounded-lg p-3 mt-10">
-                            Aanmaken
-                        </button>
-                    </div>
-                </div>
+const AddClubModal: FunctionComponent<AddClubModalData> = (
+  props: AddClubModalData
+) => {
+  return (
+    <div className={`${props.addModalOpen ? "" : "hidden"}`}>
+      <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 z-50"></div>
+      <div className="fixed top-0 left-0 w-full h-full z-50 flex justify-center items-center">
+        <div className="bg-background w-1/3 rounded-2xl p-10">
+          <div className="flex justify-between items-center text-white">
+            <h1 className="text-4xl font-semibold">Club toevoegen</h1>
+            <Icon
+              icon="mdi:close"
+              className="text-3xl hover:text-red-500 hover:cursor-pointer"
+              onClick={() => props.setAddModalOpen(!props.addModalOpen)}
+            />
+          </div>
+          <form action="/api/clubs" method="POST" className="flex flex-col">
+            <label htmlFor="name" className="text-xl text-white mt-16 mb-2">
+              Clubnaam
+            </label>
+            <input
+              type="text"
+              name="name"
+              id="name"
+              placeholder="Clubnaam"
+              className="bg-gray-200 p-2"
+            />
+            <div className="flex gap-5 justify-between">
+              <div className="flex flex-col w-1/2">
+                <label
+                  htmlFor="address_city"
+                  className="text-xl text-white mt-5 mb-2"
+                >
+                  Stad
+                </label>
+                <input
+                  type="text"
+                  name="address_city"
+                  id="address_city"
+                  placeholder="Stad"
+                  className="bg-gray-200 p-2"
+                />
+              </div>
+              <div className="flex flex-col w-1/2">
+                <label
+                  htmlFor="address_postal"
+                  className="text-xl text-white mt-5 mb-2"
+                >
+                  Postcode
+                </label>
+                <input
+                  type="text"
+                  name="address_postal"
+                  id="address_postal"
+                  placeholder="Postcode"
+                  className="bg-gray-200 p-2"
+                />
+              </div>
             </div>
+            <div className="flex gap-5 justify-between">
+              <div className="flex flex-col w-1/2">
+                <label
+                  htmlFor="address_street"
+                  className="text-xl text-white mt-5 mb-2"
+                >
+                  Straat
+                </label>
+                <input
+                  type="text"
+                  name="address_street"
+                  id="address_street"
+                  placeholder="Straat"
+                  className="bg-gray-200 p-2"
+                />
+              </div>
+              <div className="flex flex-col w-1/2">
+                <label
+                  htmlFor="address_housenumber"
+                  className="text-xl text-white mt-5 mb-2"
+                >
+                  Huisnummer
+                </label>
+                <input
+                  type="text"
+                  name="address_housenumber"
+                  id="address_housenumber"
+                  placeholder="Huisnummer"
+                  className="bg-gray-200 p-2"
+                />
+              </div>
+            </div>
+
+            <label
+              htmlFor="contactpersonid"
+              className="text-xl text-white mt-5 mb-2"
+            >
+              Contactpersoon
+            </label>
+            <select
+                name="contactpersonid"
+                id="contactpersonid"
+                className="bg-gray-200 p-2"
+                >
+                <option value="1">1</option>
+            </select>
+            <button
+              type="submit"
+              className="bg-[#0A893D] text-white rounded-lg p-3 mt-10"
+            >
+              Aanmaken
+            </button>
+          </form>
         </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
 
 export default AddClubModal;
