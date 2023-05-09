cb39bb798af9d14ea18c1e6cd3d982dfa1b49af9
Bryan <r0889617@student.thomasmore.be>
2023-05-09 12:13:03 +0200

    onrequestget by id

--- code/dfkwebsite/functions/api/clubs/[id]/index.ts
+++ code/dfkwebsite/functions/api/clubs/[id]/index.ts
@@ -6,11 +6,28 @@ export const onRequestGet: PagesFunction<PagesEnv> = async ({
   params,
 }) => {
   try {
-  } catch (e) {
-    if (e instanceof Error) {
-      return new Response(e.message);
+    const clubId = params.id.toString();
+    const clubRecord = await env.CLUBS.get(clubId);
+
+    if (!clubRecord) {
+      return new Response(JSON.stringify({ error: `Club with id ${clubId} not found` }), {
+        status: 404,
+        headers: { "content-type": "application/json" },
+      });
     }
-    return new Response("Internal server error.", { status: 500 });
+
+    const club = JSON.parse(clubRecord);
+
+    return new Response(JSON.stringify(club), {
+      headers: {
+        "content-type": "application/json",
+      },
+    });
+  } catch (e) {
+    return new Response(JSON.stringify({ error: e.message }), {
+      status: 500,
+      headers: { "content-type": "application/json" },
+    });
   }
 };
 
