b0e48eae54f39974b4f40b7cae539b8310091a03
Bryan <r0889617@student.thomasmore.be>
2023-05-09 10:14:16 +0200

    Club page fetch clubs from api

--- code/dfkwebsite/data.ts
+++ code/dfkwebsite/data.ts
@@ -1,291 +1,291 @@
-import { clubData } from "./components/Club";
+import { clubData } from "./components/ClubCard";
 import { managementData } from "./components/ManagementCard";
 import { teamData } from "./components/Team";
 import { News, Player } from "./types/general";
 
-let teams : Array<teamData> = [
-    {
-      "teamnaam": "sunt laborum",
-      "kapitein": "Lorie Soto",
-      "telefoonnummer": "(906) 523-3037"
-    },
-    {
-      "teamnaam": "mollit id",
-      "kapitein": "Alana Rutledge",
-      "telefoonnummer": "(850) 457-3899"
-    },
-    {
-      "teamnaam": "minim officia",
-      "kapitein": "Baird Fowler",
-      "telefoonnummer": "(873) 583-2043"
-    },
-    {
-      "teamnaam": "ullamco irure",
-      "kapitein": "Valerie Newman",
-      "telefoonnummer": "(981) 485-3325"
-    },
-    {
-      "teamnaam": "proident sunt",
-      "kapitein": "Morse Sparks",
-      "telefoonnummer": "(804) 548-3206"
-    },
-    {
-      "teamnaam": "reprehenderit labore",
-      "kapitein": "Cheryl Payne",
-      "telefoonnummer": "(926) 471-3203"
-    },
-    {
-      "teamnaam": "duis exercitation",
-      "kapitein": "Berry Whitfield",
-      "telefoonnummer": "(941) 597-3651"
-    },
-    {
-      "teamnaam": "quis tempor",
-      "kapitein": "Chasity Goodwin",
-      "telefoonnummer": "(909) 486-3292"
-    },
-    {
-      "teamnaam": "ullamco nulla",
-      "kapitein": "Bradford Johns",
-      "telefoonnummer": "(845) 563-3455"
-    },
-    {
-      "teamnaam": "culpa dolor",
-      "kapitein": "Case Guerra",
-      "telefoonnummer": "(927) 518-2660"
-    },
-    {
-      "teamnaam": "culpa tempor",
-      "kapitein": "Whitaker Gillespie",
-      "telefoonnummer": "(810) 481-3428"
-    },
-    {
-      "teamnaam": "et eiusmod",
-      "kapitein": "Jana Huff",
-      "telefoonnummer": "(808) 517-2063"
-    },
-    {
-      "teamnaam": "nostrud proident",
-      "kapitein": "Patton Kirk",
-      "telefoonnummer": "(964) 519-3045"
-    },
-    {
-      "teamnaam": "in minim",
-      "kapitein": "Walter Gomez",
-      "telefoonnummer": "(975) 401-3560"
-    },
-    {
-      "teamnaam": "nostrud elit",
-      "kapitein": "Kathleen Mayo",
-      "telefoonnummer": "(908) 469-2384"
-    },
-    {
-      "teamnaam": "incididunt et",
-      "kapitein": "Sandoval Mack",
-      "telefoonnummer": "(932) 584-3279"
-    },
-    {
-      "teamnaam": "reprehenderit tempor",
-      "kapitein": "Orr Rocha",
-      "telefoonnummer": "(890) 531-2605"
-    },
-    {
-      "teamnaam": "dolor ad",
-      "kapitein": "Moss Hardin",
-      "telefoonnummer": "(872) 583-2827"
-    },
-    {
-      "teamnaam": "non nisi",
-      "kapitein": "Maura Benton",
-      "telefoonnummer": "(995) 451-3130"
-    },
-    {
-      "teamnaam": "laborum fugiat",
-      "kapitein": "Brandy Andrews",
-      "telefoonnummer": "(925) 469-2708"
-    }
-  ]
+let teams: Array<teamData> = [
+  {
+    teamnaam: "sunt laborum",
+    kapitein: "Lorie Soto",
+    telefoonnummer: "(906) 523-3037",
+  },
+  {
+    teamnaam: "mollit id",
+    kapitein: "Alana Rutledge",
+    telefoonnummer: "(850) 457-3899",
+  },
+  {
+    teamnaam: "minim officia",
+    kapitein: "Baird Fowler",
+    telefoonnummer: "(873) 583-2043",
+  },
+  {
+    teamnaam: "ullamco irure",
+    kapitein: "Valerie Newman",
+    telefoonnummer: "(981) 485-3325",
+  },
+  {
+    teamnaam: "proident sunt",
+    kapitein: "Morse Sparks",
+    telefoonnummer: "(804) 548-3206",
+  },
+  {
+    teamnaam: "reprehenderit labore",
+    kapitein: "Cheryl Payne",
+    telefoonnummer: "(926) 471-3203",
+  },
+  {
+    teamnaam: "duis exercitation",
+    kapitein: "Berry Whitfield",
+    telefoonnummer: "(941) 597-3651",
+  },
+  {
+    teamnaam: "quis tempor",
+    kapitein: "Chasity Goodwin",
+    telefoonnummer: "(909) 486-3292",
+  },
+  {
+    teamnaam: "ullamco nulla",
+    kapitein: "Bradford Johns",
+    telefoonnummer: "(845) 563-3455",
+  },
+  {
+    teamnaam: "culpa dolor",
+    kapitein: "Case Guerra",
+    telefoonnummer: "(927) 518-2660",
+  },
+  {
+    teamnaam: "culpa tempor",
+    kapitein: "Whitaker Gillespie",
+    telefoonnummer: "(810) 481-3428",
+  },
+  {
+    teamnaam: "et eiusmod",
+    kapitein: "Jana Huff",
+    telefoonnummer: "(808) 517-2063",
+  },
+  {
+    teamnaam: "nostrud proident",
+    kapitein: "Patton Kirk",
+    telefoonnummer: "(964) 519-3045",
+  },
+  {
+    teamnaam: "in minim",
+    kapitein: "Walter Gomez",
+    telefoonnummer: "(975) 401-3560",
+  },
+  {
+    teamnaam: "nostrud elit",
+    kapitein: "Kathleen Mayo",
+    telefoonnummer: "(908) 469-2384",
+  },
+  {
+    teamnaam: "incididunt et",
+    kapitein: "Sandoval Mack",
+    telefoonnummer: "(932) 584-3279",
+  },
+  {
+    teamnaam: "reprehenderit tempor",
+    kapitein: "Orr Rocha",
+    telefoonnummer: "(890) 531-2605",
+  },
+  {
+    teamnaam: "dolor ad",
+    kapitein: "Moss Hardin",
+    telefoonnummer: "(872) 583-2827",
+  },
+  {
+    teamnaam: "non nisi",
+    kapitein: "Maura Benton",
+    telefoonnummer: "(995) 451-3130",
+  },
+  {
+    teamnaam: "laborum fugiat",
+    kapitein: "Brandy Andrews",
+    telefoonnummer: "(925) 469-2708",
+  },
+];
 
 export default teams;
-  
-export let bestuur : Array<managementData> = [
-    {
-      naam: "Wim Oeyen",
-      functie: "Voorzitter",
-      mail: "wim.oeyen@dfk.be",
-      telefoonnummer: "(+32) 00 123 45 67 89",
-    },
-    {
-      naam: "Pieter Fransen",
-      functie: "Secretaris",
-      mail: "pieter.fransen@dfk.be",
-      telefoonnummer: "(+32) 00 123 45 67 89",
-    },
-    {
-      naam: "Annelies Cox",
-      functie: "Penningmeester",
-      mail: "annelies.cox@dfk.be",
-      telefoonnummer: "(+32) 00 123 45 67 89"
-    },
-    {
-      naam: "Kurt Schepers",
-      functie: "Wedstrijdleiding",
-      mail: "kurt.schepers@dfk.be",
-      telefoonnummer: "(+32) 00 123 45 67 89"
-    },
-    {
-      naam: "Mario Vangeel",
-      functie: "Competitieopmaak",
-      mail:"mario.vangeel@dfk.be",
-      telefoonnummer:"(+32) 00 123 45 67 89"
-    },
-    {
-      naam: "Willy Cremers",
-      functie: "Sportieve cel/standenkeuring",
-      mail: "willy.cremers@dfk.be",
-      telefoonnummer: "(+32) 00 123 45 67 89",
-    },
-    {
-      naam: "Barry Zander",
-      functie: "Sporiteve cel/standenkeuring",
-      mail: "barry.zander@dfk.be",
-      telefoonnummer: "+31 652/71.59.60"
-    },
-    {
-      naam: "Cafe 't Centrum",
-      functie: "Ledenbeweging",
-      telefoonnummer: "014/54.97.08"
-    },
-    {
-      naam: "Jos Vanbergen",
-      functie:"Toernooileiding & sportieve cel/standkeuring",
-      mail:"jos.vanbergen@dfk.be",
-      telefoonnummer: "(+32) 00 123 45 67 89"
-    }
-  ];
 
-export let clubs : Array<clubData> = [
-    {
-      "clubnaam": "cupidatat",
-      "clubplek": "Cafe 't Centrum",
-      "straatnaam": "Madison Street",
-      "postcode": "1871"
-    },
-    {
-      "clubnaam": "aute",
-      "clubplek": "Cafe 't Centrum",
-      "straatnaam": "Norwood Avenue",
-      "postcode": "3526"
-    },
-    {
-      "clubnaam": "adipisicing",
-      "clubplek": "Cafe 't Centrum",
-      "straatnaam": "Schenck Avenue",
-      "postcode": "2038"
-    },
-    {
-      "clubnaam": "ea",
-      "clubplek": "Cafe 't Centrum",
-      "straatnaam": "Berriman Street",
-      "postcode": "3179"
-    },
-    {
-      "clubnaam": "amet",
-      "clubplek": "Cafe 't Centrum",
-      "straatnaam": "George Street",
-      "postcode": "2152"
-    },
-    {
-      "clubnaam": "sint",
-      "clubplek": "Cafe 't Centrum",
-      "straatnaam": "Clymer Street",
-      "postcode": "1345"
-    },
-    {
-      "clubnaam": "incididunt",
-      "clubplek": "Cafe 't Centrum",
-      "straatnaam": "Love Lane",
-      "postcode": "2421"
-    },
-    {
-      "clubnaam": "elit",
-      "clubplek": "Cafe 't Centrum",
-      "straatnaam": "Cadman Plaza",
-      "postcode": "2075"
-    },
-    {
-      "clubnaam": "dolor",
-      "clubplek": "Cafe 't Centrum",
-      "straatnaam": "Wythe Avenue",
-      "postcode": "1255"
-    },
-    {
-      "clubnaam": "incididunt",
-      "clubplek": "Cafe 't Centrum",
-      "straatnaam": "Gerritsen Avenue",
-      "postcode": "2813"
-    },
-    {
-      "clubnaam": "ipsum",
-      "clubplek": "Cafe 't Centrum",
-      "straatnaam": "Heath Place",
-      "postcode": "1427"
-    },
-    {
-      "clubnaam": "magna",
-      "clubplek": "Cafe 't Centrum",
-      "straatnaam": "Franklin Avenue",
-      "postcode": "1156"
-    },
-    {
-      "clubnaam": "aute",
-      "clubplek": "Cafe 't Centrum",
-      "straatnaam": "School Lane",
-      "postcode": "1689"
-    },
-    {
-      "clubnaam": "officia",
-      "clubplek": "Cafe 't Centrum",
-      "straatnaam": "Porter Avenue",
-      "postcode": "2299"
-    },
-    {
-      "clubnaam": "enim",
-      "clubplek": "Cafe 't Centrum",
-      "straatnaam": "Roder Avenue",
-      "postcode": "1670"
-    },
-    {
-      "clubnaam": "laboris",
-      "clubplek": "Cafe 't Centrum",
-      "straatnaam": "Hubbard Place",
-      "postcode": "2730"
-    },
-    {
-      "clubnaam": "quis",
-      "clubplek": "Cafe 't Centrum",
-      "straatnaam": "Provost Street",
-      "postcode": "3427"
-    },
-    {
-      "clubnaam": "non",
-      "clubplek": "Cafe 't Centrum",
-      "straatnaam": "Pooles Lane",
-      "postcode": "2946"
-    },
-    {
-      "clubnaam": "veniam",
-      "clubplek": "Cafe 't Centrum",
-      "straatnaam": "Cameron Court",
-      "postcode": "2177"
-    },
-    {
-      "clubnaam": "aute",
-      "clubplek": "Cafe 't Centrum",
-      "straatnaam": "Seaview Avenue",
-      "postcode": "1624"
-    }
-  ]
+export let bestuur: Array<managementData> = [
+  {
+    naam: "Wim Oeyen",
+    functie: "Voorzitter",
+    mail: "wim.oeyen@dfk.be",
+    telefoonnummer: "(+32) 00 123 45 67 89",
+  },
+  {
+    naam: "Pieter Fransen",
+    functie: "Secretaris",
+    mail: "pieter.fransen@dfk.be",
+    telefoonnummer: "(+32) 00 123 45 67 89",
+  },
+  {
+    naam: "Annelies Cox",
+    functie: "Penningmeester",
+    mail: "annelies.cox@dfk.be",
+    telefoonnummer: "(+32) 00 123 45 67 89",
+  },
+  {
+    naam: "Kurt Schepers",
+    functie: "Wedstrijdleiding",
+    mail: "kurt.schepers@dfk.be",
+    telefoonnummer: "(+32) 00 123 45 67 89",
+  },
+  {
+    naam: "Mario Vangeel",
+    functie: "Competitieopmaak",
+    mail: "mario.vangeel@dfk.be",
+    telefoonnummer: "(+32) 00 123 45 67 89",
+  },
+  {
+    naam: "Willy Cremers",
+    functie: "Sportieve cel/standenkeuring",
+    mail: "willy.cremers@dfk.be",
+    telefoonnummer: "(+32) 00 123 45 67 89",
+  },
+  {
+    naam: "Barry Zander",
+    functie: "Sporiteve cel/standenkeuring",
+    mail: "barry.zander@dfk.be",
+    telefoonnummer: "+31 652/71.59.60",
+  },
+  {
+    naam: "Cafe 't Centrum",
+    functie: "Ledenbeweging",
+    telefoonnummer: "014/54.97.08",
+  },
+  {
+    naam: "Jos Vanbergen",
+    functie: "Toernooileiding & sportieve cel/standkeuring",
+    mail: "jos.vanbergen@dfk.be",
+    telefoonnummer: "(+32) 00 123 45 67 89",
+  },
+];
+
+export let clubs: Array<clubData> = [
+  {
+    clubnaam: "cupidatat",
+    clubplek: "Cafe 't Centrum",
+    straatnaam: "Madison Street",
+    postcode: "1871",
+  },
+  {
+    clubnaam: "aute",
+    clubplek: "Cafe 't Centrum",
+    straatnaam: "Norwood Avenue",
+    postcode: "3526",
+  },
+  {
+    clubnaam: "adipisicing",
+    clubplek: "Cafe 't Centrum",
+    straatnaam: "Schenck Avenue",
+    postcode: "2038",
+  },
+  {
+    clubnaam: "ea",
+    clubplek: "Cafe 't Centrum",
+    straatnaam: "Berriman Street",
+    postcode: "3179",
+  },
+  {
+    clubnaam: "amet",
+    clubplek: "Cafe 't Centrum",
+    straatnaam: "George Street",
+    postcode: "2152",
+  },
+  {
+    clubnaam: "sint",
+    clubplek: "Cafe 't Centrum",
+    straatnaam: "Clymer Street",
+    postcode: "1345",
+  },
+  {
+    clubnaam: "incididunt",
+    clubplek: "Cafe 't Centrum",
+    straatnaam: "Love Lane",
+    postcode: "2421",
+  },
+  {
+    clubnaam: "elit",
+    clubplek: "Cafe 't Centrum",
+    straatnaam: "Cadman Plaza",
+    postcode: "2075",
+  },
+  {
+    clubnaam: "dolor",
+    clubplek: "Cafe 't Centrum",
+    straatnaam: "Wythe Avenue",
+    postcode: "1255",
+  },
+  {
+    clubnaam: "incididunt",
+    clubplek: "Cafe 't Centrum",
+    straatnaam: "Gerritsen Avenue",
+    postcode: "2813",
+  },
+  {
+    clubnaam: "ipsum",
+    clubplek: "Cafe 't Centrum",
+    straatnaam: "Heath Place",
+    postcode: "1427",
+  },
+  {
+    clubnaam: "magna",
+    clubplek: "Cafe 't Centrum",
+    straatnaam: "Franklin Avenue",
+    postcode: "1156",
+  },
+  {
+    clubnaam: "aute",
+    clubplek: "Cafe 't Centrum",
+    straatnaam: "School Lane",
+    postcode: "1689",
+  },
+  {
+    clubnaam: "officia",
+    clubplek: "Cafe 't Centrum",
+    straatnaam: "Porter Avenue",
+    postcode: "2299",
+  },
+  {
+    clubnaam: "enim",
+    clubplek: "Cafe 't Centrum",
+    straatnaam: "Roder Avenue",
+    postcode: "1670",
+  },
+  {
+    clubnaam: "laboris",
+    clubplek: "Cafe 't Centrum",
+    straatnaam: "Hubbard Place",
+    postcode: "2730",
+  },
+  {
+    clubnaam: "quis",
+    clubplek: "Cafe 't Centrum",
+    straatnaam: "Provost Street",
+    postcode: "3427",
+  },
+  {
+    clubnaam: "non",
+    clubplek: "Cafe 't Centrum",
+    straatnaam: "Pooles Lane",
+    postcode: "2946",
+  },
+  {
+    clubnaam: "veniam",
+    clubplek: "Cafe 't Centrum",
+    straatnaam: "Cameron Court",
+    postcode: "2177",
+  },
+  {
+    clubnaam: "aute",
+    clubplek: "Cafe 't Centrum",
+    straatnaam: "Seaview Avenue",
+    postcode: "1624",
+  },
+];
 
 // const news = Array.from({ length: 20 }, (_, i) => ({
 //     id: `${i}`,
@@ -309,7 +309,6 @@ export let clubs : Array<clubData> = [
 //   }).then(response => console.log(response));
 // });
 // }
-
 
 // const playerslist = Array.from({ length: 20 }, (_, i) => ({
 //     id: `${i}`,
