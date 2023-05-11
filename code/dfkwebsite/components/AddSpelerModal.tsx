281bd0a2070bd4e6908508792f7d09664f5213db
Bryan Deckers <r0889617@student.thomasmore.be>
2023-05-11 13:52:52 +0200

    addPlayerModal should be ready for api (not tested)

--- code/dfkwebsite/components/AddSpelerModal.tsx
+++ code/dfkwebsite/components/AddSpelerModal.tsx
@@ -1,5 +1,6 @@
 import { Icon } from "@iconify/react";
-import { FunctionComponent } from "react";
+import { FunctionComponent, useState } from "react";
+import { playerRegexPatterns } from "../modules/player";
 
 type AddSpelerModalData = {
   addModalOpen: boolean;
@@ -9,6 +10,44 @@ type AddSpelerModalData = {
 const AddSpelerModal: FunctionComponent<AddSpelerModalData> = (
   props: AddSpelerModalData
 ) => {
+  const [formValues, setFormValues] = useState<{ [key: string]: string }>({
+    name: "",
+    address_city: "",
+    address_street: "",
+    address_housenumber: "",
+    address_postal: "",
+    contactpersonid: "",
+  });
+
+  const handleChange = (event: any) => {
+    const { name, value } = event.target;
+
+    setFormValues({ ...formValues, [name]: value });
+  };
+
+  const handleSubmit = async (event: any) => {
+    // Do something with formValues, such as send it to a server
+
+    const data = new FormData();
+
+    Object.keys(formValues).forEach((formValueKey) => {
+      data.append(formValueKey, formValues[formValueKey]);
+
+      const regexPattern = playerRegexPatterns[formValueKey].regex;
+      //TODO add error box handling
+      if (regexPattern && !formValues[formValueKey].match(regexPattern)) return;
+    });
+
+    await fetch("/api/players", {
+      body: data,
+      method: "POST",
+    })
+      .then((response) => response.json())
+      .then(async (response) => {
+        //dit is uw response doe er iets mee
+      })
+      .catch((err) => console.error(err));
+  };
   return (
     <div className={`${props.addModalOpen ? "" : "hidden"}`}>
       <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 z-50"></div>
@@ -22,13 +61,7 @@ const AddSpelerModal: FunctionComponent<AddSpelerModalData> = (
               onClick={() => props.setAddModalOpen(!props.addModalOpen)}
             />
           </div>
-          <form
-            className="flex flex-col"
-            action="/api/player/add"
-            method="POST"
-            target="_self"
-            encType="multipart/form-data"
-          >
+          <div className="flex flex-col">
             <label
               htmlFor="firstname"
               className="text-xl text-white mt-16 mb-2"
@@ -40,6 +73,8 @@ const AddSpelerModal: FunctionComponent<AddSpelerModalData> = (
               name="firstname"
               id="firstname"
               placeholder="Voornaam"
+              value={formValues.name}
+              onChange={handleChange}
               className="bg-gray-200 p-2"
             />
             <label htmlFor="lastname" className="text-xl text-white mt-5 mb-2">
@@ -50,6 +85,8 @@ const AddSpelerModal: FunctionComponent<AddSpelerModalData> = (
               name="lastname"
               id="lastname"
               placeholder="Achternaam"
+              value={formValues.name}
+              onChange={handleChange}
               className="bg-gray-200 p-2"
             />
             <label htmlFor="phone" className="text-xl text-white mt-5 mb-2">
@@ -60,6 +97,8 @@ const AddSpelerModal: FunctionComponent<AddSpelerModalData> = (
               name="phone"
               id="phone"
               placeholder="Telefoonnummer"
+              value={formValues.name}
+              onChange={handleChange}
               className="bg-gray-200 p-2"
             />
             <div className="mt-5 mb-2">
@@ -71,16 +110,19 @@ const AddSpelerModal: FunctionComponent<AddSpelerModalData> = (
                 name="allowed"
                 id="allowed"
                 placeholder="Speelgerechtigd"
+                value={formValues.name}
+                onChange={handleChange}
                 className="bg-gray-200 p-2"
               />
             </div>
             <button
               type="submit"
               className="bg-[#0A893D] text-white rounded-lg p-3 mt-10"
+              onClick={handleSubmit}
             >
               Aanmaken
             </button>
-          </form>
+          </div>
         </div>
       </div>
     </div>
