6e0918140743d95c846f6e7de35af157f47a3211
Syan Delbart <syan.delbart@hotmail.com>
2023-05-13 01:37:48 +0200

    Overzicht pages more mobile-friendly by using grid, not optimal still

--- code/dfkwebsite/components/Selection.tsx
+++ code/dfkwebsite/components/Selection.tsx
@@ -8,21 +8,28 @@ type selectionData = {
   icon: string;
 };
 
-const Selectie : FunctionComponent<selectionData> = (props: selectionData) => {
+const Selectie: FunctionComponent<selectionData> = (props: selectionData) => {
   return (
-    <Link href={props.href} className="w-1/6">
+    <Link href={props.href} className="max-w-fit">
       <div className="bg-nav-background rounded-xl p-3 h-full flex flex-col">
         <div className="bg-background p-5 rounded-xl text-stone-900">
-          <Icon icon={props.icon} className={`text-8xl ${props.title === "clubs" ? "rotate-90" : ""} mx-auto`} />
+          <Icon
+            icon={props.icon}
+            className={`text-8xl ${
+              props.title === "clubs" ? "rotate-90" : ""
+            } mx-auto`}
+          />
         </div>
         <p className="capitalize text-gray-200 mt-2 text-4xl">{props.title}</p>
-        <p className="my-5 text-gray-200">Hier vind je een overzicht van {props.title}</p>
+        <p className="my-5 text-gray-200">
+          Hier vind je een overzicht van {props.title}
+        </p>
         <p className="bg-light-gray text-gray-200 rounded-md text-center py-2 mt-auto">
           Ga naar {props.title}
         </p>
       </div>
     </Link>
   );
-}
+};
 
 export default Selectie;
