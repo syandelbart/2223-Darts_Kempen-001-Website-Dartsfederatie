55f360a084a900523de188417901f03c4bd0522a
Syan Delbart <syan.delbart@hotmail.com>
2023-05-12 21:28:50 +0200

    Small changes to modal, including margin on right of title to make place for absolute close button

--- code/dfkwebsite/components/Modal.tsx
+++ code/dfkwebsite/components/Modal.tsx
@@ -2,7 +2,7 @@ import { Icon } from "@iconify/react";
 import { FunctionComponent } from "react";
 
 type ModalData = {
-  title: string;
+  title?: string;
   modalOpen: boolean;
   setModalOpen: any;
   children?: any;
@@ -18,12 +18,16 @@ const Modal: FunctionComponent<ModalData> = ({
     <div className={`${modalOpen ? "" : "hidden"}`}>
       <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 z-50"></div>
       <div className="fixed top-0 left-0 w-full h-full z-50 flex justify-center items-center">
-        <div className="bg-background w-1/4 rounded-2xl p-10">
-          <div className="flex justify-between items-center text-white">
-            <h1 className="text-4xl font-semibold">{title}</h1>
+        <div className="bg-background min-w-1/4 max-w-full rounded-2xl p-10 relative text-white">
+          <div className="flex justify-between items-center text-white relative">
+            {title ? (
+              <h1 className="text-4xl font-semibold mr-10">{title}</h1>
+            ) : (
+              ""
+            )}
             <Icon
               icon="mdi:close"
-              className="text-3xl hover:text-red-500 hover:cursor-pointer"
+              className="text-3xl hover:text-red-500 hover:cursor-pointer absolute right-0"
               onClick={() => setModalOpen(false)}
             />
           </div>
