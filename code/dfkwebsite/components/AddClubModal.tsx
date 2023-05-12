53b5ff6eedf2f6b4e5049c933a95898bbd61841b
Syan Delbart <syan.delbart@hotmail.com>
2023-05-12 21:29:04 +0200

    Components all converted to modal component

--- code/dfkwebsite/components/AddClubModal.tsx
+++ code/dfkwebsite/components/AddClubModal.tsx
@@ -2,6 +2,7 @@ import { Icon } from "@iconify/react";
 import { FunctionComponent, useState } from "react";
 import { clubRegexPatterns } from "../modules/club";
 import * as formHandler from "../modules/formHandler";
+import Modal from "./Modal";
 
 type AddClubModalData = {
   addModalOpen: boolean;
@@ -34,130 +35,121 @@ const AddClubModal: FunctionComponent<AddClubModalData> = (
   };
 
   return (
-    <div className={`${props.addModalOpen ? "" : "hidden"}`}>
-      <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 z-50"></div>
-      <div className="fixed top-0 left-0 w-full h-full z-50 flex justify-center items-center">
-        <div className="bg-background w-1/3 rounded-2xl p-10">
-          <div className="flex justify-between items-center text-white">
-            <h1 className="text-4xl font-semibold">Club toevoegen</h1>
-            <Icon
-              icon="mdi:close"
-              className="text-3xl hover:text-red-500 hover:cursor-pointer"
-              onClick={() => props.setAddModalOpen(!props.addModalOpen)}
-            />
-          </div>
-          <div className="flex flex-col">
-            <label htmlFor="name" className="text-xl text-white mt-16 mb-2">
-              Clubnaam
+    <Modal
+      title="Club toevoegen"
+      modalOpen={props.addModalOpen}
+      setModalOpen={props.setAddModalOpen}
+    >
+      <div className="flex flex-col">
+        <label htmlFor="name" className="text-xl text-white mt-16 mb-2">
+          Clubnaam
+        </label>
+        <input
+          type="text"
+          name="name"
+          id="name"
+          placeholder="Clubnaam"
+          value={formValues.name}
+          onChange={handleChange}
+          className="bg-gray-200 p-2"
+        />
+        <div className="flex gap-5 justify-between">
+          <div className="flex flex-col w-1/2">
+            <label
+              htmlFor="address_city"
+              className="text-xl text-white mt-5 mb-2"
+            >
+              Stad
             </label>
             <input
               type="text"
-              name="name"
-              id="name"
-              placeholder="Clubnaam"
-              value={formValues.name}
+              name="address_city"
+              id="address_city"
+              placeholder="Stad"
+              value={formValues.address_city}
               onChange={handleChange}
               className="bg-gray-200 p-2"
             />
-            <div className="flex gap-5 justify-between">
-              <div className="flex flex-col w-1/2">
-                <label
-                  htmlFor="address_city"
-                  className="text-xl text-white mt-5 mb-2"
-                >
-                  Stad
-                </label>
-                <input
-                  type="text"
-                  name="address_city"
-                  id="address_city"
-                  placeholder="Stad"
-                  value={formValues.address_city}
-                  onChange={handleChange}
-                  className="bg-gray-200 p-2"
-                />
-              </div>
-              <div className="flex flex-col w-1/2">
-                <label
-                  htmlFor="address_postal"
-                  className="text-xl text-white mt-5 mb-2"
-                >
-                  Postcode
-                </label>
-                <input
-                  type="text"
-                  name="address_postal"
-                  id="address_postal"
-                  value={formValues.address_postal}
-                  onChange={handleChange}
-                  placeholder="Postcode"
-                  className="bg-gray-200 p-2"
-                />
-              </div>
-            </div>
-            <div className="flex gap-5 justify-between">
-              <div className="flex flex-col w-1/2">
-                <label
-                  htmlFor="address_street"
-                  className="text-xl text-white mt-5 mb-2"
-                >
-                  Straat
-                </label>
-                <input
-                  type="text"
-                  name="address_street"
-                  value={formValues.address_street}
-                  onChange={handleChange}
-                  id="address_street"
-                  placeholder="Straat"
-                  className="bg-gray-200 p-2"
-                />
-              </div>
-              <div className="flex flex-col w-1/2">
-                <label
-                  htmlFor="address_housenumber"
-                  className="text-xl text-white mt-5 mb-2"
-                >
-                  Huisnummer
-                </label>
-                <input
-                  type="text"
-                  name="address_housenumber"
-                  value={formValues.address_housenumber}
-                  onChange={handleChange}
-                  id="address_housenumber"
-                  placeholder="Huisnummer"
-                  className="bg-gray-200 p-2"
-                />
-              </div>
-            </div>
-
+          </div>
+          <div className="flex flex-col w-1/2">
             <label
-              htmlFor="contactpersonid"
+              htmlFor="address_postal"
               className="text-xl text-white mt-5 mb-2"
             >
-              Contactpersoon
+              Postcode
             </label>
-            <select
-              name="contactpersonid"
-              id="contactpersonid"
-              value={formValues.contactpersonid}
+            <input
+              type="text"
+              name="address_postal"
+              id="address_postal"
+              value={formValues.address_postal}
               onChange={handleChange}
+              placeholder="Postcode"
               className="bg-gray-200 p-2"
-            >
-              <option value="1">1</option>
-            </select>
-            <button
-              type="submit"
-              className="bg-[#0A893D] text-white rounded-lg p-3 mt-10"
-              onClick={handleSubmit}
-            >
-              Aanmaken
-            </button>
+            />
           </div>
         </div>
+        <div className="flex gap-5 justify-between">
+          <div className="flex flex-col w-1/2">
+            <label
+              htmlFor="address_street"
+              className="text-xl text-white mt-5 mb-2"
+            >
+              Straat
+            </label>
+            <input
+              type="text"
+              name="address_street"
+              value={formValues.address_street}
+              onChange={handleChange}
+              id="address_street"
+              placeholder="Straat"
+              className="bg-gray-200 p-2"
+            />
+          </div>
+          <div className="flex flex-col w-1/2">
+            <label
+              htmlFor="address_housenumber"
+              className="text-xl text-white mt-5 mb-2"
+            >
+              Huisnummer
+            </label>
+            <input
+              type="text"
+              name="address_housenumber"
+              value={formValues.address_housenumber}
+              onChange={handleChange}
+              id="address_housenumber"
+              placeholder="Huisnummer"
+              className="bg-gray-200 p-2"
+            />
+          </div>
+        </div>
+
+        <label
+          htmlFor="contactpersonid"
+          className="text-xl text-white mt-5 mb-2"
+        >
+          Contactpersoon
+        </label>
+        <select
+          name="contactpersonid"
+          id="contactpersonid"
+          value={formValues.contactpersonid}
+          onChange={handleChange}
+          className="bg-gray-200 p-2"
+        >
+          <option value="1">1</option>
+        </select>
+        <button
+          type="submit"
+          className="bg-[#0A893D] text-white rounded-lg p-3 mt-10"
+          onClick={handleSubmit}
+        >
+          Aanmaken
+        </button>
       </div>
-    </div>
+    </Modal>
   );
 };
 
