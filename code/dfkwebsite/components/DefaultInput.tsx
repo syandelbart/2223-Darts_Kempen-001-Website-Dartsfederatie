f94f1cfa68ddf2990fb9492f99d9eb5e9e94013a
Syan Delbart <syan.delbart@hotmail.com>
2023-05-13 21:35:41 +0200

    DefaultInput component added, which will streamline input fields

---
+++ code/dfkwebsite/components/DefaultInput.tsx
@@ -0,0 +1,42 @@
+import { Icon } from "@iconify/react";
+import { ChangeEventHandler, FunctionComponent } from "react";
+import Modal from "./Modal";
+
+type DefaultInputData = {
+  type?: string;
+  id?: string;
+  placeholder?: string;
+  label?: string;
+  name: string;
+  onChange?: ChangeEventHandler<HTMLInputElement>;
+  value?: any;
+};
+
+const DefaultInput: FunctionComponent<DefaultInputData> = ({
+  type = "text",
+  name,
+  label,
+  value,
+  onChange,
+  id,
+  placeholder = "",
+}) => {
+  return (
+    <div className="flex flex-col">
+      <label htmlFor={name} className="text-xl text-white mt-5 mb-2">
+        {label ?? name}
+      </label>
+      <input
+        type={type}
+        name={name}
+        value={value}
+        id={name}
+        onChange={onChange}
+        placeholder={placeholder}
+        className="bg-gray-200 p-2 text-black"
+      />
+    </div>
+  );
+};
+
+export default DefaultInput;
