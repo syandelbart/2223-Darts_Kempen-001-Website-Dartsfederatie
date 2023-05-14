069c0a495643c9b659520f52b0aae6614cd0336c
Syan Delbart <syan.delbart@hotmail.com>
2023-05-14 11:09:57 +0200

    Made search and add button optional

--- code/dfkwebsite/components/OverzichtTopBar.tsx
+++ code/dfkwebsite/components/OverzichtTopBar.tsx
@@ -22,17 +22,25 @@ const OverzichtTopBar: FunctionComponent<topBarData> = ({
     <div className="flex justify-between items-center mb-10">
       <h1 className="text-6xl font-extrabold text-white">{titleName}</h1>
       <div className="flex gap-10 items-center">
-        <AddButton
-          name={addButtonName}
-          addModalOpen={addModalOpen}
-          setAddModalOpen={setAddModalOpen}
-        />
-        <input
-          type="text"
-          placeholder="Zoeken..."
-          className="px-5 py-3 rounded bg-[#D9D9D9]"
-          onChange={(e) => setSearch(e.target.value)}
-        />
+        {addButtonName && typeof setAddModalOpen == "function" ? (
+          <AddButton
+            name={addButtonName}
+            addModalOpen={addModalOpen}
+            setAddModalOpen={setAddModalOpen}
+          />
+        ) : (
+          ""
+        )}
+        {typeof setSearch == "function" ? (
+          <input
+            type="text"
+            placeholder="Zoeken..."
+            className="px-5 py-3 rounded bg-[#D9D9D9]"
+            onChange={(e) => setSearch(e.target.value)}
+          />
+        ) : (
+          ""
+        )}
       </div>
     </div>
   );
