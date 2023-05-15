1834f16c021d00091ed7eed132c961ffe6adb828
Bryan <r0889617@student.thomasmore.be>
2023-05-15 15:15:27 +0200

    teams by id get players api made

--- code/dfkwebsite/functions/api/teams/[id]/players.ts
+++ code/dfkwebsite/functions/api/teams/[id]/players.ts
@@ -1,3 +1,5 @@
+import { getRecordByIdOrError } from "../../../../modules/general";
+import { Team } from "../../../../types/team";
 import { PagesEnv } from "../../env";
 
 export const onRequestGet: PagesFunction<PagesEnv> = async ({
@@ -6,6 +8,27 @@ export const onRequestGet: PagesFunction<PagesEnv> = async ({
   params,
 }) => {
   try {
+    const teamId = params.id.toString();
+    const team: Team = JSON.parse(
+      await getRecordByIdOrError(teamId, env.TEAMS)
+    );
+
+    if (!team?.playersID)
+      return new Response(JSON.stringify([]), {
+        headers: {
+          "content-type": "application/json",
+        },
+      });
+
+    const playersMapped = team.playersID.map(async (playerID) => {
+      return JSON.parse(await env.PLAYERS.get(playerID));
+    });
+
+    return new Response(JSON.stringify(playersMapped), {
+      headers: {
+        "content-type": "application/json",
+      },
+    });
   } catch (e) {
     return new Response(JSON.stringify({ error: e.message }), {
       status: 500,
