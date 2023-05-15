b9c01bade76d5620c444bce285245391e27f8f2c
Bryan <r0889617@student.thomasmore.be>
2023-05-15 09:44:50 +0200

    fines by id api made without specified fields

---
+++ code/dfkwebsite/functions/api/fines/[id]/index.ts
@@ -0,0 +1,103 @@
+import { getRecordByIdOrError } from "../../../../modules/general";
+import { Fine } from "../../../../types/fine";
+import { PagesEnv } from "../../env";
+
+export const onRequestGet: PagesFunction<PagesEnv> = async ({
+  request,
+  env,
+  params,
+}) => {
+  try {
+    const fineId = params.id.toString();
+    const fine = JSON.parse(await getRecordByIdOrError(fineId, env.FINES));
+
+    return new Response(JSON.stringify(fine), {
+      headers: {
+        "content-type": "application/json",
+      },
+    });
+  } catch (e) {
+    return new Response(JSON.stringify({ error: e.message }), {
+      status: 500,
+      headers: { "content-type": "application/json" },
+    });
+  }
+};
+
+export const onRequestPut: PagesFunction<PagesEnv> = async ({
+  request,
+  env,
+  params,
+}) => {
+  try {
+    const formData = await request.formData();
+
+    const fineId = params.id.toString();
+    const fine = await getRecordByIdOrError(fineId, env.FINES);
+
+    const fineData: Fine = JSON.parse(fine);
+
+    const data: Fine = {
+      // TODO: Add the rest of the fields
+    };
+
+    // Update the fine data in the KV store
+    await env.FINES.put(fineId, JSON.stringify(data));
+
+    const responseBody = {
+      message: "Fine updated successfully.",
+      status: 200,
+    };
+
+    return new Response(JSON.stringify(responseBody), {
+      headers: { "Content-Type": "application/json" },
+    });
+  } catch (e) {
+    const errorBody = {
+      message: e instanceof Error ? e.message : "Internal server error.",
+      status: e instanceof Error ? 500 : 400,
+    };
+
+    return new Response(JSON.stringify(errorBody), {
+      headers: { "Content-Type": "application/json" },
+    });
+  }
+};
+
+export const onRequestDelete: PagesFunction<PagesEnv> = async ({
+  request,
+  env,
+  params,
+}) => {
+  try {
+    const fineId = params.id.toString();
+    const fine = await getRecordByIdOrError(fineId, env.FINES);
+
+    const fineData: Fine = JSON.parse(fine);
+
+    const data: Fine = {
+        // TODO: Add the rest of the fields
+    };
+
+    // Update the fine data in the KV store
+    await env.FINES.put(fineId, JSON.stringify(data));
+
+    const responseBody = {
+      message: "Fine deleted successfully.",
+      status: 200,
+    };
+
+    return new Response(JSON.stringify(responseBody), {
+      headers: { "Content-Type": "application/json" },
+    });
+  } catch (e) {
+    const errorBody = {
+      message: e instanceof Error ? e.message : "Internal server error.",
+      status: e instanceof Error ? 500 : 400,
+    };
+
+    return new Response(JSON.stringify(errorBody), {
+      headers: { "Content-Type": "application/json" },
+    });
+  }
+};
