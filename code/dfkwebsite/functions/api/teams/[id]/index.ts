b8d79a0eca67ac7ef350d1441b3426193ed15ed0
Bryan <r0889617@student.thomasmore.be>
2023-05-15 15:15:09 +0200

    format get team by id

--- code/dfkwebsite/functions/api/teams/[id]/index.ts
+++ code/dfkwebsite/functions/api/teams/[id]/index.ts
@@ -2,25 +2,23 @@ import { getRecordByIdOrError } from "../../../../modules/general";
 import { PagesEnv } from "../../env";
 
 export const onRequestGet: PagesFunction<PagesEnv> = async ({
-    request,
-    env,
-    params,
-  }) => {
-    try {
-      const teamId = params.id.toString();
-      const team = JSON.parse(
-        await getRecordByIdOrError(teamId, env.TEAMS)
-      );
-  
-      return new Response(JSON.stringify(team), {
-        headers: {
-          "content-type": "application/json",
-        },
-      });
-    } catch (e) {
-      return new Response(JSON.stringify({ error: e.message }), {
-        status: 500,
-        headers: { "content-type": "application/json" },
-      });
-    }
-  };
+  request,
+  env,
+  params,
+}) => {
+  try {
+    const teamId = params.id.toString();
+    const team = JSON.parse(await getRecordByIdOrError(teamId, env.TEAMS));
+
+    return new Response(JSON.stringify(team), {
+      headers: {
+        "content-type": "application/json",
+      },
+    });
+  } catch (e) {
+    return new Response(JSON.stringify({ error: e.message }), {
+      status: 500,
+      headers: { "content-type": "application/json" },
+    });
+  }
+};
