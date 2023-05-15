7188f195f83d40f4a893f07ab375fe17b618c3b5
Bryan <r0889617@student.thomasmore.be>
2023-05-15 15:14:09 +0200

    team module playersid regex added

--- code/dfkwebsite/modules/team.ts
+++ code/dfkwebsite/modules/team.ts
@@ -4,10 +4,18 @@ export enum TeamSubmission {
   NAME = "name",
   CLUB = "club", // TODO: change to clubID
   CLASSIFICATION = "classification",
+  PLAYERS = "playersID",
 }
 
 export const teamRegexPatterns: { [key: string]: fieldInformation } = {
   [TeamSubmission.NAME]: { regex: /^[a-zA-Z ]+$/, required: true },
   [TeamSubmission.CLUB]: { regex: /^[0-9]+$/, required: true },
   [TeamSubmission.CLASSIFICATION]: { regex: /^[0-9]+$/, required: true },
+  [TeamSubmission.PLAYERS]: {
+    // This regex matches the following pattern:
+    // id:xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx,id:xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx
+    regex:
+      /^(id:[a-f0-9]{8}-(?:[a-f0-9]{4}-){3}[a-f0-9]{12},)*(id:[a-f0-9]{8}-(?:[a-f0-9]{4}-){3}[a-f0-9]{12})$/,
+    required: false,
+  },
 };
