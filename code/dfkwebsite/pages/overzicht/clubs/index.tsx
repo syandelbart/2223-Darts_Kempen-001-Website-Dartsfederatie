99b19981291ba34e8692ff641243b8e55a748b9f
Syan Delbart <syan.delbart@hotmail.com>
2023-05-20 19:01:37 +0200

    Brought the modal in the page itself. The Modal now contains REAL data depending on the club that gets clicked

--- code/dfkwebsite/pages/overzicht/clubs/index.tsx
+++ code/dfkwebsite/pages/overzicht/clubs/index.tsx
@@ -5,17 +5,27 @@ import Card from "../../../components/Card";
 import CardGrid from "../../../components/CardGrid";
 import OverzichtTopBar from "../../../components/OverzichtTopBar";
 import AddClubModal from "../../../components/AddClubModal";
-import { Club } from "../../../types/club";
+import { Club, ClubFront } from "../../../types/club";
 import * as dummyData from "../../../data";
 import SelectedModal from "../../../components/SelectedModal";
 import TeamSpelers from "../../../components/TeamSpelers";
+import Modal from "../../../components/Modal";
 
 const Clubs: NextPage = () => {
-  const [clubs, setClubs] = useState<Array<Club>>(dummyData.club);
+  const [clubs, setClubs] = useState<Array<ClubFront>>(dummyData.club);
+  const [currentClub, setCurrentClub] = useState<ClubFront | null>(null);
   const [search, setSearch] = useState("");
   const [isOpen, setIsOpen] = useState(false);
   const [addModalOpen, setAddModalOpen] = useState(false);
   let results = 0;
+
+  const handleDeletePlayerFromTeam = (playerID: string, teamID: string) => {
+    // Liefste Bryan, dit is aan u! <3
+  };
+
+  const handleMakePlayerCaptain = (playerID: string, teamID: string) => {
+    // Liefste Bryan, dit is aan u! <3
+  };
 
   useEffect(() => {
     if (!process.env.NEXT_PUBLIC_NO_API) {
@@ -40,13 +50,66 @@ const Clubs: NextPage = () => {
         addModalOpen={addModalOpen}
         setAddModalOpen={setAddModalOpen}
       />
-      <SelectedModal
-        title="Selected Club"
-        isOpen={isOpen}
-        setIsOpen={setIsOpen}
-      >
-        <TeamSpelers />
-      </SelectedModal>
+
+      {currentClub && (
+        <Modal
+          title={currentClub.name}
+          modalOpen={isOpen}
+          setModalOpen={setIsOpen}
+        >
+          {currentClub.teams ? (
+            currentClub.teams.map((team) => (
+              <>
+                <div className="flex gap-3 items-center">
+                  <h1 className="text-3xl font-semibold">{team.name}</h1>
+                  <button className="bg-edit-button px-4 py-1">Edit</button>
+                </div>
+
+                <div>
+                  <div className="flex flex-col gap-2">
+                    {team.players ? (
+                      team.players.map((player) => (
+                        <div className="flex gap-3" key={player.playerID}>
+                          <p>
+                            {player.firstName} {player.lastName}
+                          </p>
+                          <button
+                            className="bg-delete-button px-8 ml-3"
+                            onClick={() =>
+                              handleDeletePlayerFromTeam(
+                                player.playerID,
+                                team.teamID
+                              )
+                            }
+                          >
+                            Verwijder van team
+                          </button>
+                          <button
+                            className="bg-edit-button px-8 text-white"
+                            onClick={() =>
+                              handleMakePlayerCaptain(
+                                player.playerID,
+                                team.teamID
+                              )
+                            }
+                          >
+                            Maak kapitein
+                          </button>
+                        </div>
+                      ))
+                    ) : (
+                      <p>Dit team heeft geen spelers</p>
+                    )}
+                  </div>
+                </div>
+              </>
+            ))
+          ) : (
+            <p>Deze club heeft geen teams.</p>
+          )}
+        </Modal>
+      )}
+
       <CardGrid>
         {clubs.length === 0 ? (
           <h1 className="text-4xl font-extrabold text-white">
@@ -64,7 +127,11 @@ const Clubs: NextPage = () => {
             })
             .map((club) => (
               <Card key={club}>
-                <ClubCard clubData={club} setIsOpen={setIsOpen} />
+                <ClubCard
+                  clubData={club}
+                  setIsOpen={setIsOpen}
+                  setCurrentClub={setCurrentClub}
+                />
               </Card>
             ))
         )}
