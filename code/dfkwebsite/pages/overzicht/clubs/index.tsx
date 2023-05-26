a94705be7535695a578342669609db638eda72bb
Bryan Deckers <r0889617@student.thomasmore.be>
2023-05-26 11:57:39 +0200

    Usage of currentmodal component for now

--- code/dfkwebsite/pages/overzicht/clubs/index.tsx
+++ code/dfkwebsite/pages/overzicht/clubs/index.tsx
@@ -14,6 +14,8 @@ import {
 import AddButton from "../../../components/AddButton";
 import AddTeamModal from "../../../components/AddTeamModal";
 import SearchableCardGrid from "../../../components/SearchableCardGrid";
+import CurrentModal from "../../../components/CurrentModal";
+import { TeamFront } from "../../../types/team";
 
 const Clubs: NextPage = () => {
   const [clubs, setClubs] = useState<Array<ClubFront>>(dummyData.club);
@@ -47,58 +49,61 @@ const Clubs: NextPage = () => {
         setAddModalOpen={setAddModalOpen}
       />
 
-      {currentClub && (
-        <Modal
-          title={currentClub.name}
-          modalOpen={isOpen}
-          setModalOpen={setIsOpen}
-        >
-          <div className="flex">
-            <input
-              className="bg-inherit"
-              type="text"
-              defaultValue={currentClub.address?.postalCode}
-            ></input>
-            <input
-              className="bg-inherit"
-              type="text"
-              defaultValue={currentClub.address?.city}
-            ></input>
-          </div>
-          <div className="flex">
-            <input
-              className="bg-inherit"
-              type="text"
-              defaultValue={currentClub.address?.street}
-            ></input>
-            <input
-              className="bg-inherit"
-              type="text"
-              defaultValue={currentClub.address?.houseNumber}
-            ></input>
-          </div>
+      <CurrentModal
+        currentObject={currentClub}
+        currentObjectKey="name"
+        currentModalOpen={isOpen}
+        setCurrentModal={setIsOpen}
+      >
+        {(club) => {
+          return (
+            <div>
+              <div className="flex">
+                <input
+                  className="bg-inherit"
+                  type="text"
+                  defaultValue={club.address?.postalCode}
+                ></input>
+                <input
+                  className="bg-inherit"
+                  type="text"
+                  defaultValue={club.address?.street}
+                ></input>
+                <input
+                  className="bg-inherit"
+                  type="text"
+                  defaultValue={club.address?.houseNumber}
+                ></input>
+                <input
+                  className="bg-inherit"
+                  type="text"
+                  defaultValue={club.address?.city}
+                ></input>
+              </div>
+              <div className="mt-10 w-1/2">
+                <AddButton
+                  name="Team toevoegen"
+                  addModalOpen={addTeamModalOpen}
+                  setAddModalOpen={setAddTeamModalOpen}
+                />
+              </div>
+              {club.teams && club.teams.length !== 0 ? (
+                club.teams.map((team: TeamFront) => (
+                  <TeamSpelers
+                    team={team}
+                    key={team.teamID}
+                    handleDeletePlayerFromTeam={handleDeletePlayerFromTeam}
+                    handleMakePlayerCaptain={handleMakePlayerCaptain}
+                  />
+                ))
+              ) : (
+                <p className="text-xl mt-10">Deze club heeft geen teams.</p>
+              )}
+            </div>
+          );
+        }}
+      </CurrentModal>
 
-          <div className="mt-10 w-1/2">
-            <AddButton
-              name="Team toevoegen"
-              addModalOpen={addTeamModalOpen}
-              setAddModalOpen={setAddTeamModalOpen}
-            />
-          </div>
-          {currentClub.teams && currentClub.teams.length !== 0 ? (
-            currentClub.teams.map((team) => (
-              <TeamSpelers
-                team={team}
-                key={team.teamID}
-                handleDeletePlayerFromTeam={handleDeletePlayerFromTeam}
-                handleMakePlayerCaptain={handleMakePlayerCaptain}
-              />
-            ))
-          ) : (
-            <p className="text-xl mt-10">Deze club heeft geen teams.</p>
-          )}
-        </Modal>
-      )}
       <AddTeamModal
         addModalOpen={addTeamModalOpen}
         setAddModalOpen={setAddTeamModalOpen}
