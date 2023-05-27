8d5936f55fb6c5e02dc788bcc4e5d69b5fad1d28
Bryan Deckers <r0889617@student.thomasmore.be>
2023-05-27 18:54:45 +0200

    Hide card delete button in management card title and add manually

--- code/dfkwebsite/components/ManagementCard.tsx
+++ code/dfkwebsite/components/ManagementCard.tsx
@@ -2,6 +2,7 @@ import { FunctionComponent } from "react";
 import CardButton from "./CardButton";
 import CardIcon from "./CardIcon";
 import CardTitle from "./CardTitle";
+import CardDeleteIcon from "./CardDeleteIcon";
 
 export type managementData = {
   naam: string;
@@ -14,10 +15,11 @@ const ManagementCard : FunctionComponent<managementData> = (props: managementDat
   return (
     <div>
       <div className="flex items-center justify-between mb-4">
-        <CardTitle>{props.naam}</CardTitle>
-        <CardButton bg={"bg-[#95A4F3]"} font={"text-[12px]"}>
+        <CardTitle hideDeleteIcon={true}>{props.naam}</CardTitle>
+        <CardButton bg={"bg-[#95A4F3]"} font={"text-[12px]"} px={"px-6"}>
           Edit
         </CardButton>
+        <CardDeleteIcon />
       </div>
       <div className="my-3">
         {props.functie ? <CardIcon icon={"ph:pen-nib"}>{props.functie}</CardIcon> : null}
