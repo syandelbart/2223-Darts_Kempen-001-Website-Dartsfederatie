36646310f14766d3674221dfd774bf360b1c979b
Bryan Deckers <r0889617@student.thomasmore.be>
2023-05-28 16:06:13 +0200

    submitbutton condition still not working

--- code/dfkwebsite/components/SubmitButton.tsx
+++ code/dfkwebsite/components/SubmitButton.tsx
@@ -12,7 +12,9 @@ const SubmitButton: FunctionComponent<SubmitButtonData> = (
   return (
     <button
       type="submit"
-      className={`${current ? "bg-[#95A4F3]" : "bg-[#0A893D]"} text-white rounded-lg p-3 mt-10`}
+      className={`${
+        current ? "bg-[#95A4F3]" : "bg-[#0A893D]"
+      } text-white rounded-lg p-3 mt-10`}
       onClick={() => handleSubmit}
     >
       {current ? "Bewerken" : "Toevoegen"}
