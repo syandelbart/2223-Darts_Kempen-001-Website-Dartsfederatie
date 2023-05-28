1eeeae23058edb4647a55cf0b555345cefa5c635
Bryan Deckers <r0889617@student.thomasmore.be>
2023-05-28 16:37:54 +0200

    fix stupid submitbutton

--- code/dfkwebsite/components/SubmitButton.tsx
+++ code/dfkwebsite/components/SubmitButton.tsx
@@ -5,17 +5,17 @@ type SubmitButtonData = {
   current?: any;
 };
 
-const SubmitButton: FunctionComponent<SubmitButtonData> = (
+const SubmitButton: FunctionComponent<SubmitButtonData> = ({
   handleSubmit,
-  current
-) => {
+  current,
+}) => {
   return (
     <button
       type="submit"
       className={`${
         current ? "bg-[#95A4F3]" : "bg-[#0A893D]"
       } text-white rounded-lg p-3 mt-10`}
-      onClick={() => handleSubmit}
+      onClick={handleSubmit}
     >
       {current ? "Bewerken" : "Toevoegen"}
     </button>
