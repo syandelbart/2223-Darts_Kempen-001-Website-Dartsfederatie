1d4f6d0beb2b4c60ba9a6e847e80f21870e14eff
Bryan Deckers <r0889617@student.thomasmore.be>
2023-05-29 12:55:38 +0200

    add button optional resetcurrent to avoid errors

--- code/dfkwebsite/components/AddButton.tsx
+++ code/dfkwebsite/components/AddButton.tsx
@@ -18,10 +18,14 @@ const AddButton: FunctionComponent<buttonData> = ({
     <div
       className="flex items-center gap-3 bg-add-button text-white rounded-lg px-5 py-3 hover:cursor-pointer"
       {...(typeof setAddModalOpen == "function" && addModalOpen != null
-        ? { onClick: () => {
-          setAddModalOpen(!addModalOpen);
-          setResetCurrent(null)
-         } }
+        ? {
+            onClick: () => {
+              setAddModalOpen(!addModalOpen);
+              {
+                setResetCurrent && setResetCurrent(null);
+              }
+            },
+          }
         : {})}
     >
       <Icon icon="fa6-solid:plus" className="text-2xl" />
