82e76a4ffbb71a92c54def67759c49a0c5ebd924
Syan Delbart <syan.delbart@hotmail.com>
2023-05-29 23:21:57 +0200

    Speeldagen no active competitions message

--- code/dfkwebsite/pages/competitie/speeldagen/index.tsx
+++ code/dfkwebsite/pages/competitie/speeldagen/index.tsx
@@ -20,37 +20,45 @@ const Speeldagen: NextPage = () => {
       <h1 className="text-6xl font-extrabold text-white mb-5">
         Speeldagen pagina
       </h1>
-      {activeCompetition.map((competition) => (
-        <div className="flex flex-col" key={competition.competitionID}>
-          <p key={competition.competitionID}>
-            {competition.competitionID} {competition.classification}
-          </p>
+      {activeCompetition?.length > 0 ? (
+        activeCompetition.map((competition) => (
+          <div className="flex flex-col" key={competition.competitionID}>
+            <p key={competition.competitionID}>
+              {competition.competitionID} {competition.classification}
+            </p>
 
-          {competition.playdays?.map((playday, i) => {
-            return (
-              <div key={i}>
-                <h2>Speeldag {i + 1}</h2>
-                <ul>
-                  {playday.map((match, j) => {
-                    return (
-                      <li key={match.team1 + match.team2}>
-                        {match.team1} vs {match.team2}
-                        <Link
-                          href={`/competitie/beheer/wedstrijdblad?competitionID=${
-                            competition.competitionID
-                          }&playdayNumber=${i + 1}&matchNumber=${j + 1}`}
-                        >
-                          Scores ingeven
-                        </Link>
-                      </li>
-                    );
-                  })}
-                </ul>
-              </div>
-            );
-          })}
-        </div>
-      ))}
+            {competition?.playdays ? (
+              competition.playdays?.map((playday, i) => {
+                return (
+                  <div key={i}>
+                    <h2>Speeldag {i + 1}</h2>
+                    <ul>
+                      {playday.map((match, j) => {
+                        return (
+                          <li key={match.team1 + match.team2}>
+                            {match.team1} vs {match.team2}
+                            <Link
+                              href={`/competitie/beheer/wedstrijdblad?competitionID=${
+                                competition.competitionID
+                              }&playdayNumber=${i + 1}&matchNumber=${j + 1}`}
+                            >
+                              Scores ingeven
+                            </Link>
+                          </li>
+                        );
+                      })}
+                    </ul>
+                  </div>
+                );
+              })
+            ) : (
+              <p>Er zijn geen speeldagen voor deze competitie</p>
+            )}
+          </div>
+        ))
+      ) : (
+        <p>Er zijn geen actieve competities ingesteld.</p>
+      )}
     </>
   );
 };
