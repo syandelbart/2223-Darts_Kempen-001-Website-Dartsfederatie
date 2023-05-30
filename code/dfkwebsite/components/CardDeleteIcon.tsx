99e5f9ba8afd07bc0d8a01adde123b99215cc1a0
Bryan <r0889617@student.thomasmore.be>
2023-05-30 17:50:17 +0200

    delete clubs functionality for now

--- code/dfkwebsite/components/CardDeleteIcon.tsx
+++ code/dfkwebsite/components/CardDeleteIcon.tsx
@@ -3,14 +3,28 @@ import { FunctionComponent } from "react";
 
 type cardTitleData = {
   target?: string;
+  id?: string;
 };
 
-const CardDeleteIcon: FunctionComponent<cardTitleData> = ({ target }) => {
+const CardDeleteIcon: FunctionComponent<cardTitleData> = ({ target, id }) => {
   return (
     <Icon
       icon="mdi:close"
       className="text-3xl hover:text-red-500 hover:cursor-pointer"
-      onClick={() => confirm(`Ben je zeker dat je ${target ? target : "dit"} wilt verwijderen?`)}
+      onClick={() => {
+        const result = confirm(
+          `Ben je zeker dat je ${target ? target : "dit"} wilt verwijderen?`
+        );
+
+        if (!result) return;
+
+        fetch(`/api/clubs/${id}`, {
+          method: "DELETE",
+        })
+          .then((res) => res.json())
+          .then((data) => console.log(data))
+          .catch((err) => console.log(err));
+      }}
     />
   );
 };
